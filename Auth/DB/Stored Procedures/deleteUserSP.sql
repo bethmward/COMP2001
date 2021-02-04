be8012c0fae473df91e51000add95707d6176b34
Bethany Marie Ward <44748486+bethmward@users.noreply.github.com>
2021-02-04 13:52:47 +0000

    Add files via upload

---
+++ Auth/DB/Stored Procedures/deleteUserSP.sql
@@ -0,0 +1,18 @@
+/* A stored procedure for the deletion of a user's details. */
+
+CREATE PROCEDURE deleteUser (@ID AS INT) AS
+BEGIN
+    BEGIN TRANSACTION
+        BEGIN TRY
+            DELETE dbo.ENDUSER
+            WHERE dbo.ENDUSER.userID = @ID;
+
+            IF @@TRANCOUNT > 0 COMMIT;
+        END TRY
+        BEGIN CATCH 
+            IF @@TRANCOUNT > 0
+                ROLLBACK TRANSACTION;
+        END CATCH;
+END;
+
+GO
---
+++ Auth/DB/Stored Procedures/updateUserSP.sql
@@ -0,0 +1,36 @@
+/* A stored procedure to update a user record. */
+
+CREATE PROCEDURE updateUser (@FirstName AS VARCHAR(25), @LastName AS VARCHAR(25), 
+    @Email AS VARCHAR(1000), @Password AS VARCHAR(25), @ID AS INT) AS
+BEGIN
+    BEGIN TRANSACTION
+        BEGIN TRY
+            IF @FirstName IS NOT NULL 
+            UPDATE dbo.ENDUSER 
+            SET dbo.ENDUSER.user_fName = @FirstName
+            WHERE dbo.ENDUSER.userID = @ID;
+            
+            IF @LastName IS NOT NULL
+            UPDATE dbo.ENDUSER
+            SET dbo.ENDUSER.user_lName = @LastName
+            WHERE dbo.ENDUSER.userID = @ID;
+
+            IF @Email IS NOT NULL
+            UPDATE dbo.ENDUSER
+            SET dbo.ENDUSER.user_email = @Email
+            WHERE dbo.ENDUSER.userID = @ID;
+
+            IF @Password IS NOT NULL
+            UPDATE dbo.USERPASSWORD
+            SET dbo.USERPASSWORD.password_new = @Password
+            WHERE dbo.USERPASSWORD.userID = @ID;
+
+            IF @@TRANCOUNT > 0 COMMIT;
+        END TRY
+        BEGIN CATCH 
+            IF @@TRANCOUNT > 0
+                ROLLBACK TRANSACTION;
+        END CATCH;
+END;
+
+GO
